a83053fbc15031e9d844e01c8ded7b74284c0c4b
Prince <padarkwah@gmail.com>
2026-02-10 00:58:02 -0500

    Fix SQLAlchemy for python 3.13

--- database.py
+++ database.py
@@ -1,12 +1,16 @@
-from sqlalchemy import create_engine, Column, Integer, String, Text, DateTime
-from sqlalchemy.orm import declarative_base
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy import create_engine, Column, Integer, Text, DateTime
+from sqlalchemy.orm import declarative_base, sessionmaker
 from datetime import datetime
 
 SQLALCHEMY_DATABASE_URL = "sqlite:///./formulations.db"
-engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False})
+
+engine = create_engine(
+    SQLALCHEMY_DATABASE_URL, 
+    connect_args={"check_same_thread": False}
+)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
 Base = declarative_base()
 
 class Formulation(Base):
